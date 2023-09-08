9daf8f942786cf58477b7bf9173769049ce815f7
evie-8 <neouma29@gmail.com>
2023-09-08 14:27:47 +0300

    corrections for 1-pack_web_static.py

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,25 +2,21 @@
 # Bash script that sets up your web servers for the deployment of web_static
 
 # Installing nginx
-# sudo -i
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo service nginx start
 
 #directories
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 
-echo '
-<html>
+sudo echo '<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-' | sudo tee /data/web_static/releases/test/index.html >/dev/null
+</html>' | sudo tee /data/web_static/releases/test/index.html >/dev/null
 
 # Creating a symbolic link
 if [ -L "/data/web_static/current" ]; then
@@ -28,6 +24,6 @@ if [ -L "/data/web_static/current" ]; then
 fi
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '15 i \\n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
