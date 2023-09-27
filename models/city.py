108c9251ee4aa196824a830f96b03d42542a9e91
evie-8 <neouma29@gmail.com>
2023-09-27 18:06:24 +0300

    corrections

--- models/city.py
+++ models/city.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """Defines fthe city class"""
 
-<<<<<<< HEAD
-from models import storage_type
-from models.base_model import BaseModel
-from sqlalchemy import Clolumn, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-=======
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
@@ -15,14 +8,12 @@ from models.state import State
 from sqlalchemy.orm import relationship
 
 
->>>>>>> de054e02cca2f4bcb0a7185a2468a28c1f596336
 class City(BaseModel, Base):
     """This is the city of the user
     Attributes:
         state_id: the state id
         name: the name of the state
     """
-<<<<<<< HEAD
     __tablename__ = 'cities'
 
     if storage_type = 'db':
@@ -33,9 +24,7 @@ class City(BaseModel, Base):
     else:
         name = ""
         state_id = ""
-=======
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     # places = relationship("Place", cascade="all, delete", backref="cities")
->>>>>>> de054e02cca2f4bcb0a7185a2468a28c1f596336
--- models/state.py
+++ models/state.py
@@ -2,33 +2,17 @@
 """Defines the state class"""
 
 
-<<<<<<< HEAD
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-=======
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 import os
 from sqlalchemy.orm import relationship
 
->>>>>>> de054e02cca2f4bcb0a7185a2468a28c1f596336
 
 class State(BaseModel, Base):
     """this is the user's state
     Attribute:
         name: the name of the state
         """
-<<<<<<< HEAD
-    __tabelname__ = 'states'
-
-    if storage_type = 'db':
-
-        name = Column(String(128), nullable=False)
-
-    else:
-
-        name = ""
-=======
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
@@ -48,4 +32,3 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     lists.append(city)
             return lists
->>>>>>> de054e02cca2f4bcb0a7185a2468a28c1f596336
